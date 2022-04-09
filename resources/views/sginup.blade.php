5e203cc3d74b70148ec15cdd059ebec17c5151c9
rawaf <mr.rar10@gmail.com>
2022-04-09 20:05:38 +0300

    Added my project

---
+++ resources/views/sginup.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form | By Code Info</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <div class="loginbox">
+        <img class="logo" src="logo.png">
+        <h1>Sign In</h1>
+        <form>
+            <label>Username</label>
+            <input type="text" placeholder="Enter Username">
+            <label>Password</label>
+            <input type="password" placeholder="Enter Password">
+            <input type="submit" value="Sign In">
+        </form>
+        <a href="#">Forgot Password?</a>
+    </div>
+</body>
+</html>
